2be3008c8204f0c88edc40b73f7f803756113692
Felipe Buso <felipe_buso@hotmail.com>
2021-05-27 19:28:36 -0300

    Item 1

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import pokemons from './data';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            position: 0,
+            limitArray: pokemons.length,
+        }
+        this.changePokemom = this.changePokemom.bind(this);
+    }
+
+    changePokemom = () => {
+        this.setState((actual) => ({
+            position: actual.position + 1
+        }))
+    }
     render() {
+        const index = this.state.position;
+
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon key={this.props.pokemons[index].id} pokemon={this.props.pokemons[index]} />
+                {/* {console.log(this)} */}
+                <button onClick={ this.changePokemom }>Rodar</button>
             </div>
         );
     }
