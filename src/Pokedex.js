d290895a41c1e723e8d402eef85b828f93105621
Felipe Buso <felipe_buso@hotmail.com>
2021-06-02 19:04:24 -0300

    continua

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -8,12 +8,15 @@ class Pokedex extends React.Component {
         this.state = {
             position: 0,
             limitArray: (pokemons.length - 1),
+            selectedType: '',
         }
         this.changePokemom = this.changePokemom.bind(this);
+        this.handleType = this.handleType.bind(this);
     }
 
     changePokemom = () => {
-        if (this.state.position < this.state.limitArray) {
+        console.log(render.newLimit);
+        if (this.state.position < this.newLimit) {
             this.setState((actual) => ({
                 position: actual.position + 1
             }))
@@ -23,16 +26,39 @@ class Pokedex extends React.Component {
             }))
         }
     }
+
+    handleType(event) {
+        // console.log(event.target.value)
+        this.setState ({
+            selectedType: event.target.value,
+            // limitArray: this.newLimit,
+        })
+    }
+
+
     render() {
         const index = this.state.position;
         const { pokemons } = this.props;
-
+        const arrayPokemonsFiltered = pokemons.filter((pokemon) => pokemon.type.includes(this.state.selectedType));
+        let newLimit = arrayPokemonsFiltered.length
         return (
             <div className="pokedex">
-                <Pokemon key={pokemons[index].id} pokemon={pokemons[index]} />
-                {/* {console.log(this)} */}
-                <button onClick={ this.changePokemom }>Rodar</button>
-                {console.log(index)};
+                <Pokemon key={arrayPokemonsFiltered[index].name} pokemon={arrayPokemonsFiltered[index]} />
+                <div className="buttons">
+                    <button onClick={this.handleType} className="select-type" value=""> All </button>
+                    <button onClick={this.handleType} className="select-type" value="Electric"> Electric </button>
+                    <button onClick={this.handleType} className="select-type" value="Fire"> Fire </button>
+                    <button onClick={this.handleType} className="select-type" value="Bug"> Bug </button>
+                    <button onClick={this.handleType} className="select-type" value="Poison"> Poison </button>
+                    <button onClick={this.handleType} className="select-type" value="Psychic"> Psychic </button>
+                    <button onClick={this.handleType} className="select-type" value="Normal"> Normal </button>
+                    <button onClick={this.handleType} className="select-type" value="Dragon"> Dragon </button>
+                </div>
+
+               <p> <button onClick={ this.changePokemom }>Rodar</button> </p>
+                {/* {console.log(newLimit)}
+                {console.log(arrayPokemonsFiltered)} */}
+                
             </div>
         );
     }
